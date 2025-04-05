eb96b792aa2aae220476c72fe0a1379ef9d03543
Akshat Goyal <goyalakshat111@gmail.com>
2025-04-05 11:04:02 +0530

    greet

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [val,setVal] = useState("");
+
+  function display(e){
+    setVal(e.target.value);
+  }
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <label>Enter your name:</label>
+       <input type="text" name="name" value={val} onChange={display}/>
+       {val &&
+        <p>Hello {val}!</p>
+       }
     </div>
   )
 }
